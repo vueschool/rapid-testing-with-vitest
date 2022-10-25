d5bf092ac187047018937bb64d472092ccf15aae
Anthony Fu <anthonyfu117@hotmail.com>
2022-10-25 16:33:02 +0800

    feat: deep merge

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,12 @@ export function deepMerge(a, b) {
   if (Array.isArray(a)) {
     return [...a, ...b]
   }
-  return Object.assign(a, b)
+  const merged = { ...a }
+  for (const key of Object.keys(b)) {
+    if (typeof a[key] === 'object' || Array.isArray(a[key]))
+      merged[key] = deepMerge(a[key], b[key])
+    else
+      merged[key] = b[key]
+  }
+  return merged
 }
