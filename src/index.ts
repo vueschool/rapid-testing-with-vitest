9ed1d12ac5000e52f00cb1dc7a0788ced8063df4
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-19 13:12:35 +0100

    chore: setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-export function deepMerge(a, b) {
-  if (Array.isArray(a) && Array.isArray(b)) {
-    return [...a, ...b]
-  }
-  if (Array.isArray(a) || Array.isArray(b) || typeof a !== typeof b) {
-    throw new Error('Can not merge two differnet types')
-  }
-  const merged = { ...a }
-  for (const key of Object.keys(b)) {
-    if (typeof a[key] === 'object' || Array.isArray(a[key]))
-      merged[key] = deepMerge(a[key], b[key])
-    else
-      merged[key] = b[key]
-  }
-  return merged
+export function greeting(name: string) {
+  console.log('Hello, ' + name)
 }
---
+++ test/spying.test.ts
@@ -0,0 +1,6 @@
+import { test, expect } from 'vitest'
+import { greeting } from '../src'
+
+test('greeting', () => {
+ 
+})
