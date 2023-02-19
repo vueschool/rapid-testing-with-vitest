2d9f70111197a395e986e12de823b0d1ecef0c8f
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-19 13:38:58 +0100

    feat: after 6.2 spying instance

--- test/spying.test.ts
+++ test/spying.test.ts
@@ -5,7 +5,29 @@ test('greeting', () => {
   const spy = vi.spyOn(console, 'log')
 
   greeting('World')
-
-  expect(spy).toBeCalledWith('Hello, World')
   expect(spy).toBeCalledTimes(1)
+  expect(spy).toBeCalledWith('Hello, World')
+
+  spy.mockReset()
+
+  greeting('Anthony')
+
+  expect(spy).toBeCalledTimes(1)
+  expect(spy).toBeCalledWith('Hello, Anthony')
+
+  expect(spy).toMatchInlineSnapshot(`
+    [MockFunction log] {
+      "calls": [
+        [
+          "Hello, Anthony",
+        ],
+      ],
+      "results": [
+        {
+          "type": "return",
+          "value": undefined,
+        },
+      ],
+    }
+  `)
 })
