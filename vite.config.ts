26e3e904af396e4e361cedc0636ad6ff66f40bc8
Anthony Fu <anthonyfu117@hotmail.com>
2023-08-26 17:28:24 +0200

    feat: vue sfc setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vitest/config'
+import Vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [
+    Vue()
+  ],
   test: {
     environment: 'happy-dom' 
   }
